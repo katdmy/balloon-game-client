48883c17ea0fd837e1fbbbcdc996e081091b49af
katdmy <dmykatkov@gmail.com>
2021-03-13 07:29:53 +1000

    Added LoginDialog and login check via SharedPreferences

--- app/src/main/java/com/katdmy/android/balloon_game_client/rooms/domain/models/RoomsPlayers.kt
+++ app/src/main/java/com/katdmy/android/balloon_game_client/rooms/domain/models/RoomsPlayers.kt
@@ -3,5 +3,6 @@ package com.katdmy.android.balloon_game_client.rooms.domain.models
 data class RoomsPlayers(
     val id: String,
     val name: String,
-    val isRoom: Boolean
+    val isRoom: Boolean,
+    val isLast: Boolean = false
 )
