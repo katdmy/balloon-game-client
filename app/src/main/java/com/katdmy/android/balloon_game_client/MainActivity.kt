0251a6549bb7fb05fc450062b58bcc1a2f1557c7
Klim <yaklim96@gmail.com>
2021-03-13 18:35:25 +0300

    add animated peppe

--- app/src/main/java/com/katdmy/android/balloon_game_client/MainActivity.kt
+++ app/src/main/java/com/katdmy/android/balloon_game_client/MainActivity.kt
@@ -1,29 +1,32 @@
 package com.katdmy.android.balloon_game_client
 
-import android.graphics.Color
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.katdmy.android.balloon_game_client.presetation.QuestionDialogFragment
 import com.katdmy.android.balloon_game_client.utils.CustomSnivel
-import com.sdsmdg.harjot.vectormaster.VectorMasterView
+
+
+private lateinit var rocketAnimation: AnimationDrawable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Меняем цвет пуза у хрюшки
-        val mrPeppe = findViewById<VectorMasterView>(R.id.vPeppe)
-        val peppesPaunch = mrPeppe.getPathModelByName("paunch")
-        peppesPaunch.fillColor = Color.parseColor("#BB0000")
+        val animatedPeppe = findViewById<ImageView>(R.id.vPeppe)
 
         val snivel = findViewById<CustomSnivel>(R.id.vSnivel)
 
         val item = findViewById<ConstraintLayout>(R.id.vPeppeWithSnivle)
 
         item.setOnClickListener {
+            val drawable = animatedPeppe.drawable
+            (drawable as Animatable).start()
             val random = (0..100).random()
             if (random in 10..90) {
                 snivel.increaseSnivel()
