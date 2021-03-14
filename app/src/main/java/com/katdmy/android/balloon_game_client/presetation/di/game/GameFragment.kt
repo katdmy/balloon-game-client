684887024ce6ee1662310b1039a614b759b72e1f
Klim <yaklim96@gmail.com>
2021-03-14 09:34:58 +0300

    launcher

--- app/src/main/java/com/katdmy/android/balloon_game_client/presetation/di/game/GameFragment.kt
+++ app/src/main/java/com/katdmy/android/balloon_game_client/presetation/di/game/GameFragment.kt
@@ -5,12 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.katdmy.android.balloon_game_client.databinding.FragmentGameBinding
-import com.katdmy.android.balloon_game_client.rooms.presentation.ViewModelFactory
 import com.katdmy.android.balloon_game_client.presetation.QuestionDialogFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
